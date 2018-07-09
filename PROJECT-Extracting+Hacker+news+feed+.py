957c87a5b14c0a471aebc0ef73b76abb69f61bcc
Nihal Singh <singhnihal1111@gmail.com>
2018-07-09 15:20:36 +0530

    Initial commit

---
+++ PROJECT-Extracting+Hacker+news+feed+.py
@@ -0,0 +1,36 @@
+
+import pandas as pd
+from bs4 import BeautifulSoup
+import urllib3
+import requests
+import warnings
+warnings.filterwarnings('ignore')
+import numpy as np
+
+
+page = requests.get('https://news.ycombinator.com/')
+
+soup = BeautifulSoup(page.content)
+
+print(soup)
+
+
+body = soup.find('table',class_='itemlist')
+
+lines = body.findAll('tr')
+
+lines[0].findAll('td',{'class':'title'})
+
+lines[0].findAll('td',{'class':'title'})
+
+heading = []
+link = []
+
+for i in range(30):
+    c=lines[3*i].findAll('td',{'class':'title'})
+    heading.append(c[1].a.text)
+    link.append(c[1].a.get('href'))
+
+df = pd.DataFrame(data= heading,columns=['Heading'],index=np.arange(1,len(heading)+1,1))
+
+df['link']= link
